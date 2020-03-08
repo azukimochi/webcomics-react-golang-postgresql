38254982e5fe6753d5929e9b48a7219622c6fb66
Karen Kua <karen.kua@points.com>
2020-03-08 12:51:38 -0400

    Cleaned up main.go by taking out unnecessary comments

--- server/main.go
+++ server/main.go
@@ -9,31 +9,8 @@ import (
 )
 
 func main() {
-	// connStr := "user=azukimochi dbname=test_db host=localhost sslmode=disable"
-	// db, err := sql.Open("postgres", connStr)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println("Started postgreSQL server successfully!")
-	// defer db.Close()
-
-	// var id int
-	// var username string
-	// name := "juny"
-	// qr, err := db.Exec("DELETE FROM users where name = $1", name)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	// fmt.Printf("Inserted successfully.  The results: %v \n", qr)
-
-	// rows, err := db.Query("SELECT id, name, username FROM users")
-	// for rows.Next() {
-	// 	rows.Scan(&id, &name, &username)
-	// 	fmt.Printf("Got: Id: %v, Name: %v, Username: %v\n", id, name, username)
-	// }
-
 	r := routes.Router()
-	fmt.Println("Starting listening on port 5000!")
 	defer controllers.DB.Close()
+	fmt.Println("Starting listening on port 5000!")
 	http.ListenAndServe(":5000", r)
 }
