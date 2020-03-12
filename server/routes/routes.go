fa6f0d1816801b1a1e47822e9e9d5eef0e7f268a
Karen Kua <karen.kua@points.com>
2020-03-11 23:40:10 -0400

    Refactored the code so that the db is opened in the router.go file in reference to an instance of a db struct.

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,18 +1,32 @@
 package routes
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+
 	"github.com/azukimochi/webcomics-react-golang-postgresql/server/controllers"
 	"github.com/gorilla/mux"
+
+	// PostgreSQL driver
+	_ "github.com/lib/pq"
 )
 
 // Router represents the routes for the API calls this app handles
 func Router() *mux.Router {
-	router := mux.NewRouter()
+	connStr := "user=azukimochi dbname=test_db host=localhost sslmode=disable"
+	DBConn, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c := controllers.CtrlDep{DB: DBConn}
+	fmt.Println("Started postgreSQL server successfully!")
 
-	router.HandleFunc("/webcomic", controllers.AddComic).Methods("POST")
-	router.HandleFunc("/webcomics", controllers.GetComics).Methods("GET")
-	router.HandleFunc("/webcomic/{id}", controllers.UpdateComic).Methods("PUT")
-	router.HandleFunc("/webcomic/{id}", controllers.DeleteComic).Methods("DELETE")
+	router := mux.NewRouter()
+	router.HandleFunc("/webcomic", c.AddComic).Methods("POST")
+	router.HandleFunc("/webcomics", c.GetComics).Methods("GET")
+	router.HandleFunc("/webcomic/{id}", c.UpdateComic).Methods("PUT")
+	router.HandleFunc("/webcomic/{id}", c.DeleteComic).Methods("DELETE")
 
 	return router
 }
